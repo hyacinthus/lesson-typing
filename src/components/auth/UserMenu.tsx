3e757c2d1db48e122e66861ed5dc59d69db08c83
hyacinthus <hyacinthus@gmail.com>
2026-02-28 02:47:31 +0800

    improve: better reg icon

--- src/components/auth/UserMenu.tsx
+++ src/components/auth/UserMenu.tsx
@@ -58,7 +58,7 @@ function EmailMaterialIcon(props: SVGProps<SVGSVGElement>) {
 function RegisterMaterialIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" {...props}>
-      <path d="M15 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8ZM7 12V9H4V7h3V4h2v3h3v2H9v3H7Zm8 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4Z" />
+      <path d="M6.25 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM3.25 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM19.75 7.5a.75.75 0 0 0-1.5 0v2.25H16a.75.75 0 0 0 0 1.5h2.25v2.25a.75.75 0 0 0 1.5 0v-2.25H22a.75.75 0 0 0 0-1.5h-2.25V7.5Z" />
     </svg>
   );
 }
